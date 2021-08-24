d6be4f5d93ff86605eb2eb4e4232297d4c3e4200
ndiritumichael <ndiritumichael5@gmail.com>
2021-08-24 21:33:08 +0300

    implemented saving pictures with mediastore

--- app/src/main/java/com/devmike/storiessaver/viewmodel/StoriesViewModel.kt
+++ app/src/main/java/com/devmike/storiessaver/viewmodel/StoriesViewModel.kt
@@ -1,13 +1,21 @@
 package com.devmike.storiessaver.viewmodel
 
 import android.app.Application
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.media.MediaMetadataRetriever
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
+import android.os.Environment
+import android.os.FileUtils
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +23,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
 import com.devmike.storiessaver.BuildConfig
 import com.devmike.storiessaver.model.STATUS_TYPE
@@ -24,20 +33,16 @@ import com.devmike.storiessaver.utils.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 
 class StoriesViewModel(application: Application):AndroidViewModel(application) {
    private val context = application.baseContext
-
-
-
-
-
-
-
-
-
+  //  val  destinationPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/statusDownloder/"
+    val  destinationPath1 = context.getExternalFilesDir(null)?.getAbsolutePath() + "/StatusDownloder/"
     val isStoragePermissionEnabled  = mutableStateOf(false)
     val videoStatus = mutableStateOf<List<Status>>(listOf())
     val imageStatus = mutableStateOf<List<Status>>(listOf())
@@ -137,6 +142,77 @@ fun updatePermssions(boolean: Boolean){
 
     fun save(status: Status) {
 
+        val filename = File(status.path)
+     val path = destinationPath1+ filename.name
+        Log.d("save",status.path)
+        try {
+            MediaStore.Images.Media.insertImage(context.getApplicationContext().getContentResolver(), BitmapFactory.decodeFile(status.path),"", "")
+            Log.d("copy Error","no error")
+        } catch (e : Exception){
+            Log.d("copy Error","error is ${e.message}")
+        }
+
+    }
+
+    fun save11(status: Status) = viewModelScope.launch(Dispatchers.Default) {
+        val bitmap = BitmapFactory.decodeFile(status.path)
+        try {
+            val fos: OutputStream?
+            var imageFile: File = File(status.path)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val resolver: ContentResolver = context.contentResolver
+                val contentValues = ContentValues()
+                contentValues.put(
+                    MediaStore.MediaColumns.DISPLAY_NAME,
+                    status.path
+
+                )
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                contentValues.put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    Environment.DIRECTORY_PICTURES + File.separator.toString() + "SavedStatuses"
+                )
+                val imageUri =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+               fos = imageUri?.let { resolver.openOutputStream(it) }
+            } else {
+                val myDir = File(
+                    Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES
+                    ).toString() + File.separator.toString() + "SavedStatuses"
+                )
+                myDir.mkdirs()
+                val filee = File(status.path)
+
+                imageFile = File(myDir,filee.name )
+                if (imageFile.exists()) imageFile.delete()
+                imageFile.createNewFile()
+                if (!imageFile.exists()) {
+                    imageFile.mkdirs()
+                }
+                MediaScannerConnection.scanFile(
+                   context,
+                    arrayOf(imageFile.toString()),
+                    null,
+                    null
+                )
+                fos = FileOutputStream(imageFile)
+
+            }
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos)
+
+
+
+
+
+        } catch (e: Exception) {
+
+            Log.d("SavingImage", e.toString())
+            withContext(Dispatchers.Main) {
+               Log.d("savingerror","error saving file")
+            }
+        }
+
     }
 
     fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
@@ -147,6 +223,10 @@ fun updatePermssions(boolean: Boolean){
                 onFinish(Color(colorValue))
             }
         }
+    }
+
+    fun getSavedFiles(){
+     val directoryPath =  Environment.DIRECTORY_PICTURES + File.separator.toString() + "SavedStatuses"
     }
 
     /*fun extractBitmap(file:File):Bitmap?{
